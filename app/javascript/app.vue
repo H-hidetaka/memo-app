e4c7b5bb3b736f5c017071e5db07e01eed2cd2bc
H-hidetaka <sansanhidetaka@gmail.com>
2021-06-16 01:51:04 +0900

    [modify]完了

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,16 +1,26 @@
 <template>
   <div id="app">
+    <div class="form">
+      <div class="form-group">
+        <input v-model="title" placeholder="タイトル" class="form-control">
+      </div>
+      <div class="form-group">
+      <!-- v-model というコードは初めて出てきました。v-model を使うことで、data () {}で定義したプロパティの値を更新できます。 -->
+        <input v-model="description" placeholder="description" class="form-control">
+      </div>
+      <button @click="addMemo">メモを追加</button>
+       <!--<button v-on:イベント名="関数"></button> 省略後<button @イベント名="関数"></button> -->
+    </div>
     <!-- v-for という書き方はVue.js特有の書き方で配列を1つずつ要素として取り出します。 -->
-    <ul>
-      <li v-for="memo in memos" :key="memo.id">
-        {{ memo.title }}: {{ memo.description }}
-      </li>
-    </ul>
-    <!-- v-model というコードは初めて出てきました。v-model を使うことで、data () {}で定義したプロパティの値を更新できます。 -->
-    <div>
-      <input v-model="title" placeholder="title">
-      <input v-model="description" placeholder="description">
-      <button @click="addMemo">メモを追加</button> <!--<button v-on:イベント名="関数"></button> 省略後<button @イベント名="関数"></button> -->
+    <div class="flex">
+      <div v-for="memo in memos" :key="memo.id" class="card">
+        <div class="card-body">
+          <div class="card-title">
+          {{ memo.title }}
+          </div>
+          {{ memo.description }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -51,9 +61,46 @@ export default {
 }
 </script>
 
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
+<style lang="scss" scoped>
+
+.form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 32px;
+  &-group {
+    margin-bottom: 1rem;
+  }
+  &-control {
+    width: 600px;
+    min-height: 24px;
+    font-size: 1rem;
+    border: 1px solid #ced4da;
+    padding: 5px 8px;
+  }
+}
+
+button {
+  width: 200px;
+}
+
+.flex {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.card {
+  width: 238px;
+  border: 1px solid rgba(0,0,0,.125);
+  margin: 16px;
+  &-body {
+    padding: 1.25ren;
+  }
+  &-title {
+    margin-bottom: .75rem;
+    font-weight: 600;
+  }
 }
 </style>
